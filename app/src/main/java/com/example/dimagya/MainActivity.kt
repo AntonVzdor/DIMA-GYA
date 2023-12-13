076d932467a70af5df56be848713c30af99b029f
dernom <der_nom@mail.ru>
2023-12-13 15:56:06 +0300

    1.1

--- app/src/main/java/com/example/dimagya/MainActivity.kt
+++ app/src/main/java/com/example/dimagya/MainActivity.kt
@@ -1,11 +1,10 @@
 package com.example.dimagya
 
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Gravity
 import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("ShowToast")
@@ -15,21 +14,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val bottonYes: Button = findViewById(R.id.YES)
-        bottonYes.setOnClickListener {
-            val textYes = "Это пока заглушка для 'ДА'"
-            val duration = Toast.LENGTH_SHORT
-
-            val toast = Toast.makeText(applicationContext, textYes, duration)
-            toast.setGravity(Gravity.CENTER, 0, 0)
-        }
-
         val bottonNo: Button = findViewById(R.id.NO)
-        bottonNo.setOnClickListener {
-            val textNo = "Это пока заглушка для 'НЕТ'"
-            val duration = Toast.LENGTH_SHORT
 
-            val toast = Toast.makeText(applicationContext, textNo, duration)
-            toast.setGravity(Gravity.CENTER, 0, 0)
+        bottonYes.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Заглушка для 'ДА'", Toast.LENGTH_SHORT).show()
         }
+
+        bottonNo.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Заглушка для 'НЕТ'", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
+
