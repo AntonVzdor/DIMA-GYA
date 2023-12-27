dc821882b246b2dc0353511ea5befa2f97401311
Anton <antderar1488_antnoman2517@mail.ru>
2023-12-27 13:55:50 +0300

    1,5 Кнопки работают

--- app/src/main/java/com/example/dimagya/MainActivity.kt
+++ app/src/main/java/com/example/dimagya/MainActivity.kt
@@ -1,30 +1,25 @@
 package com.example.dimagya
 
-import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("ShowToast")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
 
-        val bottonYes: Button = findViewById(R.id.YES)
-        val bottonNo: Button = findViewById(R.id.NO)
+        val bottomYes: Button = findViewById(R.id.YES)
+        val bottomNo: Button = findViewById(R.id.NO)
 
-        bottonYes.setOnClickListener {
-            Toast.makeText(this@MainActivity, "Заглушка для 'ДА'", Toast.LENGTH_SHORT).show()
+        bottomYes.setOnClickListener {
+            startActivity(Intent(this, YesActivity::class.java))
         }
-
-        bottonNo.setOnClickListener {
-            Toast.makeText(this@MainActivity, "Заглушка для 'НЕТ'", Toast.LENGTH_SHORT).show()
+        bottomNo.setOnClickListener {
+            startActivity(Intent(this, NoActivity::class.java))
         }
-
     }
 }
-
-//коммент
